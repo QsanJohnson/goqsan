aea9b41899b8ecc944ba2282391e6d5785f530ef
QsanKyle <115699413+QsanKyle@users.noreply.github.com>
2022-12-09 17:34:13 +0800

    add metadata and snapshot
    1.add Get/Patch metadata
    2.add Get/Patch timestamp
    3.add snapshot

--- volume.go
+++ volume.go
@@ -4,6 +4,7 @@ package goqsan
 
 import (
 	"context"
+	b64 "encoding/base64"
 	"encoding/json"
 	"net/http"
 )
@@ -13,6 +14,12 @@ type VolumeOp struct {
 	client *AuthClient
 }
 
+// type Metadata struct {
+// 	Status  string `json:"status"`
+// 	Type    string `json:"type"`
+// 	Content string `json:"content"`
+// }
+
 type VolumeData struct {
 	ID                    string `json:"id"`
 	Name                  string `json:"name"`
@@ -20,6 +27,7 @@ type VolumeData struct {
 	LunID                 string `json:"lunId"`
 	TargetID              string `json:"targetId"`
 	Online                bool   `json:"online"`
+	State                 string `json:"state"`
 	Health                string `json:"health"`
 	Provision             string `json:"provision"`
 	TotalSize             uint64 `json:"totalSize"`
@@ -39,17 +47,31 @@ type VolumeData struct {
 		Wwn  string `json:"wwn"`
 		Type string `json:"type"`
 	} `json:"tags"`
+	VDMMetadata struct {
+		Status    string `json:"status"`
+		Type      string `json:"type"`
+		Content   string `json:"content"`
+		Timestamp string `json:"timestamp"`
+	} `json:"metadata"`
+}
+
+type MetadataStruct struct {
+	Status    string `json:"status,omitempty"`
+	Type      string `json:"type,omitempty"`
+	Content   string `json:"content,omitempty"`
+	Timestamp string `json:"timestamp,omitempty"`
 }
 
 type VolumeCreateOptions struct {
-	Name            string `json:"name"`
-	TotalSize       uint64 `json:"totalSize"`
-	BlockSize       uint64 `json:"blockSize"`
-	PoolID          string `json:"poolId"`
-	IoPriority      string `json:"ioPriority,omitempty"`
-	BgIoPriority    string `json:"bgIoPriority,omitempty"`
-	CacheMode       string `json:"cacheMode,omitempty"`
-	EnableReadAhead *bool  `json:"enableReadAhead,omitempty"`
+	Name            string         `json:"name"`
+	TotalSize       uint64         `json:"totalSize"`
+	BlockSize       uint64         `json:"blockSize"`
+	PoolID          string         `json:"poolId"`
+	IoPriority      string         `json:"ioPriority,omitempty"`
+	BgIoPriority    string         `json:"bgIoPriority,omitempty"`
+	CacheMode       string         `json:"cacheMode,omitempty"`
+	EnableReadAhead *bool          `json:"enableReadAhead,omitempty"`
+	Metadata        MetadataStruct `json:"metadata,omitempty"`
 }
 
 //Patch /rest/v2/storage/block/volumes/_volumes
@@ -67,12 +89,13 @@ type VolumeQoSOptions struct {
 //Patch /rest/v2/storage/block/volumes/_volumes
 type VolumeModifyOptions struct {
 	VolumeQoSOptions
-	Name            string `json:"name,omitempty"`
-	TotalSize       uint64 `json:"totalSize,omitempty"`
-	BgIoPriority    string `json:"bgIoPriority,omitempty"`
-	CacheMode       string `json:"cacheMode,omitempty"`
-	EnableReadAhead *bool  `json:"enableReadAhead,omitempty"`
-	Tags            Tag    `json:"tags,omitempty"`
+	Name            string         `json:"name,omitempty"`
+	TotalSize       uint64         `json:"totalSize,omitempty"`
+	BgIoPriority    string         `json:"bgIoPriority,omitempty"`
+	CacheMode       string         `json:"cacheMode,omitempty"`
+	EnableReadAhead *bool          `json:"enableReadAhead,omitempty"`
+	Tags            Tag            `json:"tags,omitempty"`
+	Metadata        MetadataStruct `json:"metadata,omitempty"`
 }
 
 // type VolumeModifyOptions struct {
@@ -94,6 +117,66 @@ type QoSData struct {
 // NewVolume returns volume operation
 func NewVolume(client *AuthClient) *VolumeOp {
 	return &VolumeOp{client}
+}
+
+// return value of GET /rest/v2/backup/snapshot/targets/_volumeID
+// return value of PATCH /rest/v2/backup/snapshot/targets/_volumeID
+type VolSnaphshotSetting struct {
+	Type              string `json:"type"`
+	SnapshotMaxPolicy struct {
+		MaxLimit uint64 `json:"maxLimit"`
+		Policy   string `json:"policy"`
+	} `json:"snapshotMaxPolicy"`
+	ProtectionGroup string `json:"protectionGroup"`
+	TotalSize       uint64 `json:"totalSize"`
+}
+
+// return value of GET /rest/v2/backup/snapshot/targets/_volumeID/snapshots
+// return value of Post /rest/v2/backup/snapshot/targets/_volumeID/snapshots
+// return value of Patch /rest/v2/backup/snapshot/targets/_volumeID/snapshots/_snapshotID
+type VolSnaphshotLists struct {
+	ID         string `json:"id"`
+	Name       string `json:"name"`
+	CreateTime string `json:"createTime"`
+	UsedSize   uint64 `json:"usedSize"`
+	Expose     struct {
+		Enable    bool        `json:"enable"`
+		Mode      interface{} `json:"mode"`
+		WriteSize uint64      `json:"writeSize"`
+	} `json:"expose"`
+	Trash struct {
+		InTrash    bool        `json:"inTrash"`
+		DeleteTime interface{} `json:"deleteTime"`
+	} `json:"trash"`
+}
+
+// Post /rest/v2/backup/snapshot/targets/_volumeID/snapshots
+type VolumeSnapshotName struct {
+	Name string `json:"name,omitempty"`
+}
+
+// Patch /rest/v2/backup/snapshot/targets/_volumeID
+type VolumeSnapshotPatchSetting struct {
+	ProtectionGroup string `json:"protectionGroup,omitempty"`
+	TotalSize       int    `json:"totalSize,omitempty"`
+}
+
+// Patch /rest/v2/backup/snapshot/targets/_volumeID/snapshots/_snapshotID
+type ExposeStruct struct {
+	Enable    bool   `json:"enable"`
+	Mode      string `json:"mode"`
+	WriteSize uint64 `json:"writeSize"`
+}
+
+// Patch /rest/v2/backup/snapshot/targets/_volumeID/snapshots/_snapshotID
+type TrashStruct struct {
+	InTrash bool `json:"inTrash"`
+}
+
+// Patch /rest/v2/backup/snapshot/targets/_volumeID/snapshots/_snapshotID
+type VolSnaphshotOptions struct {
+	Expose ExposeStruct `json:"expose"`
+	Trash  TrashStruct  `json:"trash"`
 }
 
 // ListVolumes list all volumes
@@ -226,4 +309,235 @@ func (v *VolumeOp) PatchQoS(ctx context.Context, qosEnable bool, qosRule string)
 		return nil, err
 	}
 	return &res, nil
+}
+
+// Get Volume snapshot settings
+// GET /rest/v2/backup/snapshot/targets/_volumeID
+func (v *VolumeOp) GetVolumeSnapshotSetting(ctx context.Context, volId string) (*VolSnaphshotSetting, error) {
+
+	req, err := v.client.NewRequest(ctx, http.MethodGet, "/rest/v2/backup/snapshot/targets/"+volId, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res := VolSnaphshotSetting{}
+	if err := v.client.SendRequest(ctx, req, &res); err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+// Enable snapshot space
+// PATCH /rest/v2/backup/snapshot/targets/_volumeID
+func (v *VolumeOp) PatchVolumeSnapshotSetting(ctx context.Context, volId string, options *VolumeSnapshotPatchSetting) (*VolSnaphshotSetting, error) {
+
+	rawdata, _ := json.Marshal(options)
+	req, err := v.client.NewRequest(ctx, http.MethodPatch, "/rest/v2/backup/snapshot/targets/"+volId, string(rawdata))
+	if err != nil {
+		return nil, err
+	}
+
+	res := VolSnaphshotSetting{}
+	if err := v.client.SendRequest(ctx, req, &res); err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+// Create volume snapshot
+// POST /rest/v2/backup/snapshot/targets/_volumeID/snapshots/_snapshotName
+func (v *VolumeOp) CreateVolumeSnapshotLists(ctx context.Context, volId string, options *VolumeSnapshotName) (*VolSnaphshotLists, error) {
+
+	rawdata, _ := json.Marshal(options)
+	req, err := v.client.NewRequest(ctx, http.MethodPost, "/rest/v2/backup/snapshot/targets/"+volId+"/snapshots", string(rawdata))
+	if err != nil {
+		return nil, err
+	}
+
+	res := VolSnaphshotLists{}
+	if err := v.client.SendRequest(ctx, req, &res); err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+// Get Volume snapshot lists
+// GET /rest/v2/backup/snapshot/targets/_volumeID/snapshots
+func (v *VolumeOp) GetVolumeSnapshotLists(ctx context.Context, volId string) (*[]VolSnaphshotLists, error) {
+
+	req, err := v.client.NewRequest(ctx, http.MethodGet, "/rest/v2/backup/snapshot/targets/"+volId+"/snapshots", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res := []VolSnaphshotLists{}
+	if err := v.client.SendRequest(ctx, req, &res); err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+// Get Volume certain snapshot list
+// GET /rest/v2/backup/snapshot/targets/_volumeID/snapshots/_snapshotID
+func (v *VolumeOp) GetVolumeSnapshotList(ctx context.Context, volId, snapId string) (*VolSnaphshotLists, error) {
+
+	req, err := v.client.NewRequest(ctx, http.MethodGet, "/rest/v2/backup/snapshot/targets/"+volId+"/snapshots/"+snapId, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res := VolSnaphshotLists{}
+	if err := v.client.SendRequest(ctx, req, &res); err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+// Patch certain volume snapshot
+// PATCH /rest/v2/backup/snapshot/targets/_volumeID/snapshots/_snapshotID
+func (v *VolumeOp) PatchVolumeSnapshot(ctx context.Context, volId, snapId string, options *VolSnaphshotOptions) (*[]VolSnaphshotLists, error) {
+
+	rawdata, _ := json.Marshal(options)
+	req, err := v.client.NewRequest(ctx, http.MethodPatch, "/rest/v2/backup/snapshot/targets/"+volId+"/snapshots/"+snapId, string(rawdata))
+	if err != nil {
+		return nil, err
+	}
+
+	res := []VolSnaphshotLists{}
+	if err := v.client.SendRequest(ctx, req, &res); err != nil {
+		return nil, err
+	}
+
+	return &res, nil
+}
+
+// Rollback to certain volume snapshot
+// POST /rest/v2/backup/snapshot/targets/_volumeID/snapshots/_snapshotID
+func (v *VolumeOp) RollbackVolumeSnapshot(ctx context.Context, volId, snapId string) error {
+	req, err := v.client.NewRequest(ctx, http.MethodPost, "/rest/v2/backup/snapshot/targets/"+volId+"/snapshots/"+snapId+"/rollback", nil)
+	if err != nil {
+		return err
+	}
+
+	res := EmptyData{}
+	if err := v.client.SendRequest(ctx, req, &res); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Delete all volume snapshots
+// DELETE /rest/v2/backup/snapshot/targets/_volumeID/snapshots
+func (v *VolumeOp) DeleteVolumeSnapshots(ctx context.Context, volId string) error {
+	req, err := v.client.NewRequest(ctx, http.MethodDelete, "/rest/v2/backup/snapshot/targets/"+volId+"/snapshots", nil)
+	if err != nil {
+		return err
+	}
+
+	res := EmptyData{}
+	if err := v.client.SendRequest(ctx, req, &res); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Delete certain volume snapshot
+// DELETE /rest/v2/backup/snapshot/targets/_volumeID/snapshots/_snapshotID
+func (v *VolumeOp) DeleteVolumeSnapshot(ctx context.Context, volId, snapId string) error {
+	req, err := v.client.NewRequest(ctx, http.MethodDelete, "/rest/v2/backup/snapshot/targets/"+volId+"/snapshots/"+snapId, nil)
+	if err != nil {
+		return err
+	}
+
+	res := EmptyData{}
+	if err := v.client.SendRequest(ctx, req, &res); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Get metadata Timestamp
+func (v *VolumeOp) GetMetadataTimestamp(ctx context.Context, volId string) (string, error) {
+
+	req, err := v.client.NewRequest(ctx, http.MethodGet, "/rest/v2/storage/block/volumes/"+volId, nil)
+	if err != nil {
+		return "", err
+	}
+
+	res := VolumeData{}
+	if err := v.client.SendRequest(ctx, req, &res); err != nil {
+		return "", err
+	}
+	return res.VDMMetadata.Timestamp, nil
+}
+
+// update metadata Timestamp
+func (v *VolumeOp) PatchMetadataTimestamp(ctx context.Context, volId, timestamp string) (string, error) {
+
+	param := &VolumeModifyOptions{
+		Metadata: MetadataStruct{
+			Timestamp: timestamp,
+		},
+	}
+	rawdata, _ := json.Marshal(param)
+
+	req, err := v.client.NewRequest(ctx, http.MethodPatch, "/rest/v2/storage/block/volumes/"+volId, string(rawdata))
+	if err != nil {
+		return "", err
+	}
+
+	res := VolumeData{}
+	if err := v.client.SendRequest(ctx, req, &res); err != nil {
+		return "", err
+	}
+	return res.VDMMetadata.Timestamp, nil
+}
+
+// Get metadata
+func (v *VolumeOp) GetMetadata(ctx context.Context, volId string) (string, string, []byte, error) {
+
+	req, err := v.client.NewRequest(ctx, http.MethodGet, "/rest/v2/storage/block/volumes/"+volId, nil)
+	if err != nil {
+		return "", "", nil, err
+	}
+
+	res := VolumeData{}
+	if err := v.client.SendRequest(ctx, req, &res); err != nil {
+		return "", "", nil, err
+	}
+
+	rawDecodedText, _ := b64.StdEncoding.DecodeString(res.VDMMetadata.Content)
+
+	return res.VDMMetadata.Status, res.VDMMetadata.Type, []byte(rawDecodedText), nil
+}
+
+// Update metadata
+func (v *VolumeOp) PatchMetadata(ctx context.Context, volId, metastatus, metatype string, metacontent []byte) (string, string, []byte, error) {
+
+	metacontent64 := b64.StdEncoding.EncodeToString(metacontent)
+	param := &VolumeModifyOptions{
+		Metadata: MetadataStruct{
+			Status:  metastatus,
+			Type:    metatype,
+			Content: metacontent64,
+		},
+	}
+	rawdata, _ := json.Marshal(param)
+
+	req, err := v.client.NewRequest(ctx, http.MethodPatch, "/rest/v2/storage/block/volumes/"+volId, string(rawdata))
+	if err != nil {
+		return "", "", nil, err
+	}
+
+	res := VolumeData{}
+	if err := v.client.SendRequest(ctx, req, &res); err != nil {
+		return "", "", nil, err
+	}
+
+	rawDecodedText, _ := b64.StdEncoding.DecodeString(res.VDMMetadata.Content)
+
+	return res.VDMMetadata.Status, res.VDMMetadata.Type, []byte(rawDecodedText), nil
 }
--- volume_test.go
+++ volume_test.go
@@ -2,6 +2,7 @@ package goqsan
 
 import (
 	"context"
+	b64 "encoding/base64"
 	"fmt"
 	"net/http"
 	"testing"
@@ -17,12 +18,22 @@ func TestVolume(t *testing.T) {
 	//TRUE := true
 	FALSE := false
 
+	//byte test for metadata
+	metabyte := []byte{48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64}
+	metabyte64 := b64.StdEncoding.EncodeToString(metabyte)
+	pmetabyte := []byte{48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 97, 98, 99, 100, 101}
+
 	paramCVol := VolumeCreateOptions{
 		BlockSize:       4096,
 		IoPriority:      "HIGH",
 		BgIoPriority:    "HIGH",
 		CacheMode:       "WRITE_THROUGH",
 		EnableReadAhead: &FALSE,
+		Metadata: MetadataStruct{
+			Status:  "VALID",
+			Type:    "CSI Driver",
+			Content: metabyte64,
+		},
 	}
 
 	//patch QoS settings
@@ -37,18 +48,35 @@ func TestVolume(t *testing.T) {
 	}
 
 	// options2 := VolumeModifyOptions{
-	// 	Name:                 "afterMod",
-	// 	IoPriority:           "MEDIUM",
-	// 	BgIoPriority: "LOW",
-	// 	CacheMode:            "WRITE_BACK",
-	// 	EnableReadAhead:      &TRUE,
+	// 	Name:            "afterMod",
+	// 	IoPriority:      "MEDIUM",
+	// 	BgIoPriority:    "LOW",
+	// 	CacheMode:       "WRITE_BACK",
+	// 	EnableReadAhead: &TRUE,
 	// }
 
+	// create snapshot name
+	paramSnapName := VolumeSnapshotName{
+		Name: "kyle_snap1",
+	}
+	// assign snapshot space
+	paramSnapSpace := VolumeSnapshotPatchSetting{
+		TotalSize: 20480,
+	}
+
+	//create, list, delete
 	now := time.Now()
 	timeStamp := now.Format("20060102150405")
 	volName := "gotest-vol-" + timeStamp
 	createDeleteVolumeTest(t, testConf.poolId, volName, 5120, &paramCVol)
 
+	//create, list, get metadata, patch metadata, get timestamp, list timestamp, delete
+	now = time.Now()
+	timeStamp = now.Format("20060102150405")
+	volName = "gotest-vol-" + timeStamp
+	metaDataTest(t, testConf.poolId, volName, 5120, "1670571795", pmetabyte, &paramCVol)
+
+	//create, patch volume, delete
 	now = time.Now()
 	timeStamp = now.Format("20060102150405")
 	volName = "gotest-vol-" + timeStamp
@@ -59,6 +87,11 @@ func TestVolume(t *testing.T) {
 	qosTest(t, true, "IO_PRIORITY")
 	qosTest(t, false, "IO_PRIORITY")
 	qosTest(t, true, "MAX_IOPS_THROUGHPUT")
+
+	now = time.Now()
+	timeStamp = now.Format("20060102150405")
+	volName = "gotest-vol-" + timeStamp
+	snapshotTest(t, testConf.poolId, volName, 10240, &paramCVol, &paramSnapSpace, &paramSnapName)
 
 }
 
@@ -126,6 +159,76 @@ func createDeleteVolumeTest(t *testing.T, poolID, volname string, volsize uint64
 		t.Fatalf("ListVolumeByID failed: %v", err)
 	}
 	fmt.Printf("ListVolumeByID : %+v \n", v)
+
+	fmt.Printf("  Sleep 5 seconds\n")
+	time.Sleep(5 * time.Second)
+
+	//delete volume
+	fmt.Println("start delete")
+	err = testConf.volumeOp.DeleteVolume(ctx, vol.ID)
+	if err != nil {
+		t.Fatalf("DeleteVolume failed: %v", err)
+	}
+	fmt.Printf("  A volume was deleted. Id:%s\n", vol.ID)
+
+	fmt.Println("createDeleteVolumeTest Leave")
+}
+
+func metaDataTest(t *testing.T, poolID, volname string, volsize uint64, ptimestamp string, pMetabyte []byte, options *VolumeCreateOptions) {
+	fmt.Printf("createDeleteVolumeTest Enter (volSize: %d,  %+v )\n", options.TotalSize, *options)
+
+	//create volume
+	vol, err := testConf.volumeOp.CreateVolume(ctx, poolID, volname, volsize, options)
+	if err != nil {
+		t.Fatalf("createVolume failed: %v", err)
+	}
+	fmt.Printf("  A volume was created. Id:%s \n", vol.ID)
+
+	//list volume
+	v, err := testConf.volumeOp.ListVolumeByID(ctx, vol.ID)
+	if err != nil {
+		resterr, ok := err.(*RestError)
+		if ok {
+			if resterr.StatusCode == http.StatusNotFound {
+				t.Fatalf("Volume %s not found.", vol.ID)
+			}
+			fmt.Printf("[ListVolumeByID] StatusCode=%d ErrResp=%+v\n", resterr.StatusCode, resterr.ErrResp)
+		}
+
+		t.Fatalf("ListVolumeByID failed: %v", err)
+	}
+	fmt.Printf("ListVolumeByID : %+v \n", v)
+
+	//Get metadata
+	metaStatus, metaType, metaContent, err := testConf.volumeOp.GetMetadata(ctx, vol.ID)
+	if err != nil {
+		t.Fatalf("Get Metadata failed: %v", err)
+	}
+	fmt.Printf(" metadata Status is :%s \n", metaStatus)
+	fmt.Printf(" metadata Type is :%s \n", metaType)
+	fmt.Printf(" metadata Content is :%b \n", metaContent)
+
+	metaStatus, metaType, metaContent, err = testConf.volumeOp.PatchMetadata(ctx, vol.ID, metaStatus, metaType, pMetabyte)
+	if err != nil {
+		t.Fatalf("Update Metadata failed: %v", err)
+	}
+	fmt.Printf(" New metadata Status is :%s \n", metaStatus)
+	fmt.Printf(" New metadata Type is :%s \n", metaType)
+	fmt.Printf(" New metadata Content is :%b \n", metaContent)
+
+	//Get metadata timestamp
+	tstamp, err := testConf.volumeOp.GetMetadataTimestamp(ctx, vol.ID)
+	if err != nil {
+		t.Fatalf("Get Timestamp failed: %v", err)
+	}
+	fmt.Printf(" metadata timestamp is :%s \n", tstamp)
+
+	//update metadata timestamp
+	tstamp, err = testConf.volumeOp.PatchMetadataTimestamp(ctx, vol.ID, ptimestamp)
+	if err != nil {
+		t.Fatalf("Update Timestamp failed: %v", err)
+	}
+	fmt.Printf("Updated metadata timestamp is :%s \n", tstamp)
 
 	fmt.Printf("  Sleep 5 seconds\n")
 	time.Sleep(5 * time.Second)
@@ -294,4 +397,122 @@ func qosTest(t *testing.T, qosEnable bool, qosRule string) {
 	}
 
 	fmt.Println("QoSTest Leave")
+}
+
+func snapshotTest(t *testing.T, poolID, volname string, volsize uint64, optionsV *VolumeCreateOptions, optionsSP *VolumeSnapshotPatchSetting, optionsSN *VolumeSnapshotName) {
+	fmt.Printf("snapshotTest Enter \n")
+	fmt.Printf("createVolume (volSize: %d,  %+v )\n", optionsV.TotalSize, *optionsV)
+
+	//create volume
+	vol, err := testConf.volumeOp.CreateVolume(ctx, poolID, volname, volsize, optionsV)
+	if err != nil {
+		t.Fatalf("createVolume failed: %v", err)
+	}
+	fmt.Printf("  A volume was created. Id:%s \n", vol.ID)
+
+	fmt.Printf("  Sleep 10 seconds\n")
+	time.Sleep(10 * time.Second)
+
+	//get volume snapshot settings
+	snapSet, err := testConf.volumeOp.GetVolumeSnapshotSetting(ctx, vol.ID)
+	if err != nil {
+		t.Fatalf("Get volume snapshot setting failed: %v", err)
+	}
+	fmt.Printf("Volume snapshot settings: %v \n", snapSet)
+
+	//enable snapshot center and assign space
+	//patch volume snapshot settings
+	snapPat, err := testConf.volumeOp.PatchVolumeSnapshotSetting(ctx, vol.ID, optionsSP)
+	if err != nil {
+		t.Fatalf("Enable snapshot center failed: %v", err)
+	}
+	fmt.Printf("Snapshot center enabled, with space: %d \n", snapPat.TotalSize)
+
+	fmt.Printf("  Sleep 5 seconds\n")
+	time.Sleep(5 * time.Second)
+
+	//create volume snapshot
+	snapC, err := testConf.volumeOp.CreateVolumeSnapshotLists(ctx, vol.ID, optionsSN) //need to be changed to body input
+	if err != nil {
+		t.Fatalf("createVolume snapshot failed: %v", err)
+	}
+	fmt.Printf("  A volume snapshot was created. Snapshot Id:%s \n", snapC.ID)
+
+	//get volume snapshot lists
+	snaplist, err := testConf.volumeOp.GetVolumeSnapshotLists(ctx, vol.ID)
+	if err != nil {
+		t.Fatalf("Get volume snapshot lists failed: %v", err)
+	}
+	fmt.Printf("Volume snapshot lists: %v \n", snaplist)
+
+	fmt.Printf("  Sleep 3 seconds\n")
+	time.Sleep(3 * time.Second)
+
+	//get certain volume snapshot lists
+	snapName2 := VolumeSnapshotName{
+		Name: "kyle_snap2",
+	}
+	snapC2, err := testConf.volumeOp.CreateVolumeSnapshotLists(ctx, vol.ID, &snapName2) //need to be changed to body input
+	if err != nil {
+		t.Fatalf("createVolume snapshot failed: %v", err)
+	}
+	fmt.Printf("  A volume snapshot was created. Snapshot Id:%s \n", snapC2.ID)
+
+	snaplist2, err := testConf.volumeOp.GetVolumeSnapshotList(ctx, vol.ID, snapC2.ID)
+	if err != nil {
+		t.Fatalf("Get certain volume snapshot list failed: %v", err)
+	}
+	fmt.Printf("snapshot ID: %s 's list: %v \n", snapC2.ID, snaplist2)
+
+	fmt.Printf("  Sleep 5 seconds\n")
+	time.Sleep(5 * time.Second)
+
+	//Rollback to the first snapshot
+	err = testConf.volumeOp.RollbackVolumeSnapshot(ctx, vol.ID, snapC.ID)
+	if err != nil {
+		t.Fatalf("Rollback to first snapshot failed: %v", err)
+	}
+	//check if rollback to first snapshot will delete the rest snapshot
+	snaplist, err = testConf.volumeOp.GetVolumeSnapshotLists(ctx, vol.ID)
+	if err != nil {
+		t.Fatalf("Get volume snapshot lists failed: %v", err)
+	}
+	if len(*snaplist) != 1 {
+		t.Fatalf("Rollback to first snapshot applied, but there are still more than one snapshot left.")
+	}
+	fmt.Printf("Rollback to the first snapshot, ID: %s . \n", snapC.ID)
+
+	fmt.Printf("  Sleep 5 seconds\n")
+	time.Sleep(5 * time.Second)
+
+	//delete snapshot
+	fmt.Println("start delete volume snapshot")
+	err = testConf.volumeOp.DeleteVolumeSnapshots(ctx, vol.ID)
+	if err != nil {
+		t.Fatalf("Delete Volume snapshots failed: %v", err)
+	}
+	fmt.Printf("  All snapshots were deleted. \n")
+
+	//disable snapshot center
+	optionsDisable := VolumeSnapshotPatchSetting{
+		TotalSize: 0,
+	}
+	snapPat, err = testConf.volumeOp.PatchVolumeSnapshotSetting(ctx, vol.ID, &optionsDisable)
+	if err != nil {
+		t.Fatalf("Disable snapshot center failed: %v", err)
+	}
+	fmt.Printf("Snapshot center disabled, space now is: %d \n", snapPat.TotalSize)
+
+	fmt.Printf("  Sleep 5 seconds\n")
+	time.Sleep(5 * time.Second)
+
+	//delete volume
+	fmt.Println("start delete volume")
+	err = testConf.volumeOp.DeleteVolume(ctx, vol.ID)
+	if err != nil {
+		t.Fatalf("DeleteVolume failed: %v", err)
+	}
+	fmt.Printf("  A volume was deleted. Id:%s\n", vol.ID)
+
+	fmt.Println("snapshotTest Leave")
 }
